7288f6f3b36b8618a07fc7139401bffc14cfa934
StoKou <2781856148@qq.com>
2024-05-13 23:13:56 +0800

    2024_3_31_1

--- 2024_3_31/1.cpp
+++ 2024_3_31/1.cpp
@@ -1,8 +1,29 @@
 #include<iostream>
+#include<cstring>
 using namespace std;
 
+int art[101]={0};
+int flag[101]={0};
+int numcout[101]={0};
 int main(){
+    int n,m,k,x;
+    cin>>n>>m;
+    for(int i=1;i<=n;i++){
+        cin>>k;
+        memset(flag,0,sizeof(flag));
+        for(int j=1;j<=k;j++){
+            cin>>x;
+            numcout[x]++;
+            if(flag[x]==0){
+                art[x]++;
+                flag[x]++;
+            }
+        }
+    }
 
-    cout<<"success!";
+    for(int i=1;i<=m;i++){
+        cout<<art[i]<<" "<<numcout[i]<<" "<<endl;
+    }
     return 0;
 }
+
