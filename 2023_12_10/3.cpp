a69830f36294f8960b557f287065b588ed71e84a
StoKou <2781856148@qq.com>
2024-05-21 09:40:41 +0800

    3_cpp

---
+++ 2023_12_10/3.cpp
@@ -0,0 +1,32 @@
+#inlcude<bits/stdc++.h>
+using namespace std;
+
+
+int weight[2001]{0};
+int W[2001]{0};
+int parent[2001]{0};//
+int TotalWeight=0;
+void UpdateW(){
+
+}
+int GetMinW(){
+    UpdateW();
+
+}
+void getD(){
+    for(int i=1;i<=n;i++){
+        scanf("%d",&weight[i])
+        TotalWeight+=weight[i];
+    }
+    for(int i=2;i<=n;i++){
+        scanf("%d",&parent[i]);
+    }
+}
+int main(){
+    int n,m;
+    scanf("%d%d",&n,&m);
+    for(int i=1;i<=m;i++){
+
+    }
+
+}
